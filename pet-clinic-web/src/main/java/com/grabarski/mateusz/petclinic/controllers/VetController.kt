7f46abe197b219b0e02d0c1dca65f0a91778a8e9
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-10-16 12:55:36 +0200

    added vets controller

---
+++ pet-clinic-web/src/main/java/com/grabarski/mateusz/petclinic/controllers/VetController.kt
@@ -0,0 +1,13 @@
+package com.grabarski.mateusz.petclinic.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class VetController {
+
+    @RequestMapping("/vets", "/vets/index", "/vets/index.html")
+    fun listVets(): String {
+        return "vets/index"
+    }
+}
